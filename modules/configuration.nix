ae89bda5a8c6c8af3768ed22bfacad956e8255a1
Volodymyr Antonov <azazaka2002@gmail.com>
2023-07-17 22:20:17 +0300

    hardware: add printers support

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -86,6 +86,13 @@
     pulse.enable = true;
   };
 
+  # Enable avahi
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+    openFirewall = true;
+  };
+
   security.rtkit.enable = true;
   security.polkit.enable = true;
 
@@ -124,6 +131,7 @@
     extraBackends = [ pkgs.sane-airscan ];
   };
   services.ipp-usb.enable = true;
+  services.printing.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
