9bc488118460f9355b35a00b120a068a3dfe74ee
Volodymyr Antonov <azazaka2002@gmail.com>
2024-01-27 17:10:09 +0200

    hyprland: add monitor

--- programs/hyprland.nix
+++ programs/hyprland.nix
@@ -4,8 +4,13 @@
   package = hyprland;
   settings = {
     monitor = [
-      "eDP-1, 1920x1080, 0x0, 1.15"
+      "eDP-1, 1920x1080, 1920x0, 1.15"
+      "HDMI-A-1, 1920x1080@100, 0x0, 1"
     ];
+
+    misc = {
+      vrr = 1;
+    };
 
     input = {
       kb_layout = "us, ua";
