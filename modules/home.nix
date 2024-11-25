2c0a8c836755c945061cd6ab0269e83a242a8f04
Volodymyr Antonov <azazaka2002@gmail.com>
2024-11-25 13:10:37 +0200

    network-manager-applet: init

--- modules/home.nix
+++ modules/home.nix
@@ -175,6 +175,8 @@
         };
       };
 
+      services.network-manager-applet.enable = true;
+
       # This value determines the Home Manager release that your
       # configuration is compatible with. This helps avoid breakage
       # when a new Home Manager release introduces backwards
