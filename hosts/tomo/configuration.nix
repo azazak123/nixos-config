565eb0325913f80b62c5cc6609a99ecc232485e1
Volodymyr Antonov <azazaka2002@gmail.com>
2025-03-12 23:23:12 +0200

    nix: move config to separate file

--- hosts/tomo/configuration.nix
+++ hosts/tomo/configuration.nix
@@ -10,6 +10,10 @@
   ...
 }:
 
+let
+  programs = ../../programs;
+in
+
 {
   imports = [
     # Include the results of the hardware scan.
@@ -121,34 +125,7 @@
   security.rtkit.enable = true;
   security.polkit.enable = true;
 
-  nix = {
-    # Use the same nixpkgs for nix flake commands
-    registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
-
-    # Use the same nixpkgs for nix non flake commands
-    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
-
-    settings = {
-      # Enable flakes
-      experimental-features = [
-        "nix-command"
-        "flakes"
-      ];
-
-      # Deduplicate and optimize nix store
-      auto-optimise-store = true;
-
-      trusted-public-keys = [ "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=" ];
-      trusted-substituters = [ "https://devenv.cachix.org" ];
-    };
-
-    # Enable auto garbage collecting
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 30d";
-    };
-  };
+  nix = import /${programs}/nix-config.nix { inherit inputs lib config; };
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
