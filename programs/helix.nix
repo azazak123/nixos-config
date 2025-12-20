f1f9cf35d2534300ad68e7c5205ef1352af98912
Volodymyr Antonov <azazaka2002@gmail.com>
2025-12-20 20:27:41 +0200

    helix: disable arrows

--- programs/helix.nix
+++ programs/helix.nix
@@ -1,7 +1,40 @@
 {
   enable = true;
+  defaultEditor = true;
   settings = {
     theme = "material_palenight";
-    editor.line-number = "relative";
+    editor = {
+    line-number = "relative";
+      cursor-shape = {
+        normal = "block";      
+        insert = "bar";        
+        select = "underline";  
+      };
+    };
+    keys = {
+      normal = {
+        up = "no_op";
+        down = "no_op";
+        left = "no_op";
+        right = "no_op";
+        
+        pageup = "no_op";
+        pagedown = "no_op";
+      };
+
+      select = {
+        up = "no_op";
+        down = "no_op";
+        left = "no_op";
+        right = "no_op";
+      };
+
+      insert = {
+        up = "no_op";
+        down = "no_op";
+        left = "no_op";
+        right = "no_op";
+      };
+    };
   };
 }
