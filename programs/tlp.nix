d3dcdfd7900eeecc775e54ed8e6c4959fa6cc4d4
Volodymyr Antonov <azazaka2002@gmail.com>
2026-02-28 14:18:55 +0200

    tlp: decrease power consumption

--- programs/tlp.nix
+++ programs/tlp.nix
@@ -1,14 +1,16 @@
 {
   enable = true;
   settings = {
-    RUNTIME_PM_ON_AC = "on";
+    RUNTIME_PM_ON_AC = "auto";
+    RUNTIME_PM_ON_BAT = "auto";
+    PCIE_ASPM_ON_BAT = "powersupersave";
 
     CPU_DRIVER_OPMODE_ON_AC = "guided";
     CPU_DRIVER_OPMODE_ON_BAT = "guided";
 
     CPU_SCALING_GOVERNOR_ON_AC = "performance";
-    CPU_SCALING_GOVERNOR_ON_BAT = "schedutil";
-    CPU_SCALING_GOVERNOR_ON_SAV = "schedutil";
+    CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+    CPU_SCALING_GOVERNOR_ON_SAV = "powersave";
 
     CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
     CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
@@ -17,7 +19,9 @@
     RADEON_DPM_STATE_ON_AC = "performance";
     RADEON_DPM_STATE_ON_BAT = "battery";
 
-    USB_AUTOSUSPEND = "0";
+    RADEON_DPM_PERF_LEVEL_ON_BAT = "low";
+
+    USB_AUTOSUSPEND = "1";
 
     CPU_BOOST_ON_AC = "1";
     CPU_BOOST_ON_BAT = "0";
@@ -28,5 +32,11 @@
     CPU_SCALING_MAX_FREQ_ON_AC = "3900000";
     CPU_SCALING_MAX_FREQ_ON_BAT = "3900000";
     CPU_SCALING_MAX_FREQ_ON_SAV = "3900000";
+
+    SOUND_POWER_SAVE_ON_AC = "0";
+    SOUND_POWER_SAVE_ON_BAT = "1";
+    SOUND_POWER_SAVE_CONTROLLER = "Y";
+
+    NMI_WATCHDOG = "0";
   };
 }
