642d76fc856e8415afc7c37840fcd01a069a3b69
Volodymyr Antonov <azazaka2002@gmail.com>
2026-04-03 00:53:50 +0300

    firefox(azazak123): set settings

--- users/azazak123.nix
+++ users/azazak123.nix
@@ -112,8 +112,67 @@ in
       programs.home-manager.enable = true;
 
       # Browsers
-      programs.firefox.enable = true;
-      programs.floorp.enable = true;
+      stylix.targets.firefox.profileNames = [ "azazak123" ];
+      stylix.targets.floorp.profileNames = [ "azazak123" ];
+
+      programs.floorp = {
+        enable = true;
+        profiles.azazak123 = {
+          isDefault = true;
+          settings = {
+            "browser.startup.page" = 3;
+            "browser.sessionstore.interval" = 600000;
+            "browser.download.useDownloadDir" = false;
+            "browser.tabs.closeWindowWithLastTab" = false;
+            "signon.rememberSignons" = false;
+            "privacy.donottrackheader.enabled" = true;
+            "network.dns.disablePrefetch" = true;
+            "network.prefetch-next" = false;
+            "network.http.speculative-parallel-limit" = 0;
+            "toolkit.telemetry.enabled" = false;
+            "toolkit.telemetry.unified" = false;
+            "browser.ping-centre.telemetry" = false;
+            "experiments.supported" = false;
+            "network.allow-experiments" = false;
+            "privacy.userContext.enabled" = true;
+            "privacy.userContext.ui.enabled" = true;
+            "media.ffmpeg.vaapi.enabled" = true;
+          };
+        };
+      };
+
+      programs.firefox = {
+        enable = true;
+        profiles.azazak123 = {
+          isDefault = true;
+          settings = {
+            "browser.startup.page" = 3;
+            "browser.sessionstore.interval" = 600000;
+            "browser.download.useDownloadDir" = false;
+            "browser.tabs.closeWindowWithLastTab" = false;
+            "signon.rememberSignons" = false;
+            "privacy.donottrackheader.enabled" = true;
+            "network.dns.disablePrefetch" = true;
+            "network.prefetch-next" = false;
+            "network.http.speculative-parallel-limit" = 0;
+            "toolkit.telemetry.enabled" = false;
+            "toolkit.telemetry.unified" = false;
+            "browser.ping-centre.telemetry" = false;
+            "experiments.supported" = false;
+            "network.allow-experiments" = false;
+            "privacy.userContext.enabled" = true;
+            "privacy.userContext.ui.enabled" = true;
+            "media.ffmpeg.vaapi.enabled" = true;
+            "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+          };
+
+          userChrome = ''
+            #TabsToolbar {
+              visibility: collapse !important;
+            }
+          '';
+        };
+      };
 
       # Wayland / UI
       wayland.windowManager.hyprland = import /${programs}/hyprland.nix { inherit (pkgs) hyprland; };
@@ -147,7 +206,7 @@ in
       };
 
       services.ssh-agent.enable = true;
-      
+
       # Emacs
       services.emacs.enable = false;
       programs.emacs = {
