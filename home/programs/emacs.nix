0b25530ad68ca48772370fd319d2f4ee4ec0b09e
Volodymyr Antonov <azazaka2002@gmail.com>
2026-04-11 17:42:13 +0300

    emacs: enable daemon

--- home/programs/emacs.nix
+++ home/programs/emacs.nix
@@ -1,9 +1,14 @@
 { pkgs, ... }:
 
 {
-  services.emacs.enable = false;
+
+  services.emacs = {
+    enable = true;
+    startWithUserSession = "graphical";
+  };
+
   stylix.targets.emacs.enable = false;
-  
+
   programs.emacs = {
     enable = true;
     package = pkgs.emacs-pgtk;
@@ -11,11 +16,13 @@
       epkgs.treesit-grammars.with-all-grammars
     ];
   };
-  
-  xdg.configFile."emacs/init.el".source = ../dotfiles/emacs/init.el;
-  xdg.configFile."emacs/early-init.el".source = ../dotfiles/emacs/early-init.el;
-  xdg.configFile."emacs/modules".source = ../dotfiles/emacs/modules;
-  
+
+  xdg.configFile = {
+    "emacs/init.el".source = ../dotfiles/emacs/init.el;
+    "emacs/early-init.el".source = ../dotfiles/emacs/early-init.el;
+    "emacs/modules".source = ../dotfiles/emacs/modules;
+  };
+
   # programs.doom-emacs = {
   #   enable = true;
   #   doomDir = ../dotfiles/doom.d;
